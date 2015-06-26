d38f0b766b6b98d824cf1149bd7e9655f1407c15
jaquelinecm <Jaqueline@Jaque.local>
2015-06-26 11:20:42 -0300

    colocando o push
    para rodar faça ionic serve —lab

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic','ngCordova','ionic.service.core','ionic.service.push','starter.controllers','starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,15 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(['$ionicAppProvider', function($ionicAppProvider) {
+  // Identify app
+  $ionicAppProvider.identify({
+    // The App ID (from apps.ionic.io) for the server
+    app_id: 'e30fbc59',
+    // The public API key all services will use for this app
+    api_key: '7a8da0a3d5b224af24e848449a449a99dc2453f761aa2ba1',
+    // Set the app to use development pushes
+    dev_push: true
+  });
+}])
