180ce42294a3603fddeed8edd816930d6f15bd6f
André Giampaolo <andregiampaolo@gmail.com>
2015-07-15 17:33:43 -0300

    Incluindo dependências do banco de dados
    Incluindo configurações para utilização do banco de dados

--- www/js/app.js
+++ www/js/app.js
@@ -4,9 +4,9 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('enejApp', ['ionic'])
+angular.module('enejApp', ['ionic', 'ngCordova'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $cordovaSQLite) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -17,6 +17,14 @@ angular.module('enejApp', ['ionic'])
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    
+    if (window.cordova) {
+      db = $cordovaSQLite.openDB({ name: "my.db" }); //device
+    }else{
+      db = window.openDatabase("my.db", '1', 'my', 1024 * 1024 * 100); // browser
+    }
+    $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS people (id integer primary key, firstname text, lastname text)");
+    
   });
 })
 
